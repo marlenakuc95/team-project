fd2d9dccef25b9fedc4d662d9fe6d701143ed2b7
Humorloos <l.loos95@gmail.com>
2021-07-10 17:43:18 +0200

    Add some experiments with SPARQL service

--- bio_ontology_annotator.py
+++ bio_ontology_annotator.py
@@ -4,10 +4,10 @@ import urllib.parse
 from pprint import pprint
 import pandas as pd
 import rdflib
-
+from SPARQLWrapper import SPARQLWrapper, RDFXML, XML
+import urllib
 import requests
 
-g = rdflib.Graph()
 REST_URL = "http://data.bioontology.org"
 API_KEY = '598d2acb-d88f-41d8-909e-fe7d79d74dfa'
 
@@ -28,12 +28,37 @@ def load_json(url):
 
 
 resources = load_json(REST_URL)
-a = load_json('https://data.bioontology.org/ontologies/OCHV/classes/http%3A%2F%2Fsbmi.uth.tmc.edu%2Fontology%2Fochv%23C0025202/mappings')
 text_to_annotate = 'Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in ' \
-                   'the bowel and the eye. '
+                   'the bowel and the eye.'
 
 annotations = load_json(resources['links']['annotator'] + f'?text={urllib.parse.quote(text_to_annotate)}')
+
+
+def describe(uri):
+    params = urllib.parse.urlencode({'query': f"""DESCRIBE <{uri}>""", 'apikey': API_KEY})
+
+    return json.loads(
+        requests.get(f"http://sparql.bioontology.org/sparql?{params}",
+                     headers={'Accept': 'application/json'}).content)
+
+
+unique_ids = list({annotation['annotatedClass']['@id'] for annotation in annotations})
+
+sparql_descriptions = {id: describe(id) for id in unique_ids}
+
+description_lengths = {key: len(value) for key, value in sparql_descriptions.items()}
+description_lengths_df = pd.DataFrame(description_lengths.items(), columns=['uri', 'found'])
+description_lengths_df['found'] = description_lengths_df['found'].astype(bool)
+found_uris = description_lengths_df[description_lengths_df['found']]['uri']
+uris_not_found = description_lengths_df[~description_lengths_df['found']]['uri']
+
+description_lengths = {next(iter(value.keys())): len(next(iter(value.values()))) for value in
+                       sparql_descriptions.values() if len(value) > 0}
+a = sparql_descriptions['http://purl.obolibrary.org/obo/UBERON_0000019']
+
 parents = load_json(annotations[0]['annotatedClass']['links']['parents'])
+
+instances = load_json(annotations[0]['annotatedClass']['links']['ancestors'])
 #
 a = pd.DataFrame([{
     'text': annotation['annotations'][0]['text'],
@@ -46,8 +71,6 @@ a['id'].nunique()
 {len(annotation['annotations']) for annotation in annotations}
 {len(annotation['hierarchy']) for annotation in annotations}
 {annotation['annotations'][0]['text'] for annotation in annotations}
-
-a = {annotation['annotatedClass']['@id'] for annotation in annotations}
 
 pprint(annotations[1])
 
