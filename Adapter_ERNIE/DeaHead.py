bfbd2bd2c1421d1423986af7da61f936883ebca8
Humorloos <l.loos95@gmail.com>
2021-08-30 18:53:05 +0200

    Add mock implementation of AdapterErnie

---
+++ Adapter_ERNIE/DeaHead.py
@@ -0,0 +1,7 @@
+import pytorch_lightning as pl
+
+
+class DeaHead(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        # TODO: Implement dEA head
---
+++ Adapter_ERNIE/train.py
@@ -0,0 +1,38 @@
+import torch
+from pytorch_lightning.callbacks import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+
+from AdapterErnie import AdapterErnie
+import pytorch_lightning as pl
+
+from utils import DATA_DIR
+
+MIN_DELTA = 0.01  # minimum delta in validation loss for early stopping
+PATIENCE = 3  # number of consecutive epochs with validation loss < MIN_DELTA after which to stop early
+
+config = {
+    'adapter_type': 'pfeiffer',
+    'adapter_non_linearity': 'relu',
+    'adapter_reduction_factor': 16,
+    'batch_size_train': 2,
+    'num_workers': 1,
+    'optimizer': torch.optim.AdamW,
+    'lr': 1e-2,
+    'weight_decay': 1e-3
+}
+
+model = AdapterErnie(config=config)
+# next(iter(model.train_dataloader()))
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    min_delta=MIN_DELTA,
+    patience=PATIENCE,
+    verbose=True)
+# train model
+trainer = pl.Trainer(
+    logger=TensorBoardLogger(save_dir=DATA_DIR, name="", version="."),
+    callbacks=[early_stopping],
+    gpus=torch.cuda.device_count(),
+)
+if __name__ == "__main__":
+    trainer.fit(model)
