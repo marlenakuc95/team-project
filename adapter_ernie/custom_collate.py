e1e5cec361390d4a69b1e49b80751cb5e197e13e
Humorloos <l.loos95@gmail.com>
2021-10-26 11:07:01 +0200

    Add logging and remove unnecessary outputs

--- adapter_ernie/custom_collate.py
+++ adapter_ernie/custom_collate.py
@@ -1,3 +1,5 @@
+import logging
+
 import torch
 
 
@@ -7,23 +9,22 @@ def custom_collate(batch):
     Args:
      batch: (list of tuples) [(input_ids, attention_mask, alignments, embeddings)].
          input_ids - fixed size
-         attention_mask - fixed size
          alignments - variable length
          embeddings - variable length
     """
+    logging.info('Collating batch')
     # The size of the input_ids will always be the same fixed to =512.
     # Size of alignment matrix and embeddings will be different, as number of entities found vary between inputs.
     # One of the solution is to pad the matrices to the fixed size in the batch.
 
     if len(batch) == 1:
         input_ids = batch[0][0].unsqueeze(0)
-        attention_mask = batch[0][1].unsqueeze(0)
         alignments = batch[0][2].unsqueeze(0)
         embeddings = batch[0][3].unsqueeze(0)
 
     else:
-        input_ids, attention_mask, alignments, embeddings = zip(
-            *[(a, b, c, d) for (a, b, c, d) in sorted(batch, key=lambda tup: tup[2].size(0), reverse=True)])
+        input_ids, alignments, embeddings = zip(
+            *[(a, b, c) for (a, b, c) in sorted(batch, key=lambda tup: tup[1].size(0), reverse=True)])
 
         # Padding with 0 or -1? I guess -1?
         max_len = alignments[0].size(0)
@@ -40,6 +41,6 @@ def custom_collate(batch):
         alignments = torch.stack(alignments, 0)
         embeddings = torch.stack(embeddings, 0)
         input_ids = torch.tensor(input_ids)
-        attention_mask = torch.tensor(attention_mask)
 
-    return input_ids, attention_mask, alignments, embeddings
+    logging.info('Loading collated batch')
+    return input_ids, alignments, embeddings
