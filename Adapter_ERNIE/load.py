26086714c0b891283d7b148461de23dfd34794ba
Marlena <57439171+marlenakuc95@users.noreply.github.com>
2021-12-22 01:50:02 +0100

    Delete Adapter_ERNIE directory

--- Adapter_ERNIE/load.py
+++
@@ -1,7 +0,0 @@
-from AdapterErnie import AdapterErnie
-from constants import PROJECT_DIR
-
-#%%
-checkpoint_path = PROJECT_DIR / "Adapter_ERNIE" / "wandb" / "adapter-ernie" /\
-                  "22oymqen" / "checkpoints" / "epoch=3-step=176663.ckpt"
-model = AdapterErnie.load_from_checkpoint(checkpoint_path)
